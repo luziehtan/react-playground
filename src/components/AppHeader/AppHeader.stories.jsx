5d70edcaf9aaa789fcf1bc5eb5c7f407fc5c4151
Luciana Blümlein <luziehtan@hotmail.com>
2021-02-15 15:03:26 +0100

    Changes in js files

---
+++ src/components/AppHeader/AppHeader.stories.jsx
@@ -0,0 +1,9 @@
+import AppHeader from './AppHeader'
+import React from 'react'
+
+export default {
+  title: 'AppHeader',
+  component: AppHeader,
+}
+
+export const JS = () => <AppHeader title="Hi Storybook!" />
